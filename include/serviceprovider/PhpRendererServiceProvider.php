9300f09167be9d564958ec15d5e6a84f8543766a
Ludovic Henry <ludovichenry.utbm@gmail.com>
2014-01-29 18:59:53 +0100

    Create PHP renderer service provider. Use to render PHP templates.
    Example :
    
    require_once(__DIR__ . "/../include/serviceprovider/PhpRendererServiceProvider.php");
    
    $app->register(new Silex\Provider\PhpRendererServiceProvider());
    
    $content = $app['renderer']->render(
    	__DIR__ . '/views/template.php',
    	array(
    		'request' => $request,
    		'site'       => $site,
    		'key'       => $value,
    	)
    );

---
+++ include/serviceprovider/PhpRendererServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Silex\Provider;
+
+require_once __DIR__.'/../../vendor/silexautoload.php';
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class PhpRendererServiceProvider implements ServiceProviderInterface {
+
+    public function register(Application $app) {
+      $app['renderer'] = $this;
+    }
+
+    public function boot(Application $app) {
+    }
+
+    public function render($filename, array $variables) {
+      foreach ($variables as $key => $value) {
+        $$key = $value;
+      }
+
+      ob_start();
+
+      include $filename;
+
+      return ob_get_clean();
+    }
+}
