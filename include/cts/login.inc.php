b1eac9edf291e566e8fc88afb498d5179e07674b
Ludovic Henry <ludovichenry.utbm@gmail.com>
2014-02-01 21:27:58 +0100

    Forget old connect.php link in include/cts/login.inc.php file.

--- include/cts/login.inc.php
+++ include/cts/login.inc.php
@@ -48,7 +48,7 @@ class loginerror extends board
 
     $this->board("Veuillez vous identifier","loginerror");
 
-    $frm = new form("connect2",$wwwtopdir."connect.php",true,"POST","Vous avez déjà un compte");
+    $frm = new form("connect2",$wwwtopdir."connexion.php",true,"POST","Vous avez déjà un compte");
     $frm->add_select_field(
         "domain",
         "Connexion",
