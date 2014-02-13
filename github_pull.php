a4c3567056d4027bfd177f053c53e00ea01bd530
Ludovic Henry <ludovichenry.utbm@gmail.com>
2014-02-14 00:34:39 +0100

    Fix github_pull.php script

--- github_pull.php
+++ github_pull.php
@@ -8,43 +8,45 @@ use Symfony\Component\HttpFoundation\Response;
 $app = new Silex\Application();
 $app['debug'] = false;
 
-$app->post('/', function (Request $request) use ($app) {
-    $remoteip    = ip2long($_SERVER['REMOTE_ADDR']);
-    $allowednet  = ip2long("192.30.252.0");
-    $allowedmask = ip2long("255.255.252.0");
-    
-    if ($remoteip === false || ($remoteip & $allowedmask) != $allowednet) {
-      $app->abort(403, "Vous n'êtes pas les serveurs GitHub");
-    }
+$app->error(function (\Exception $e, $code) use ($app) {
+  if ($app['debug']) {
+    return;
+  }
 
-    $commands = array(
-      "git --git-dir=" . __DIR__ . "/.git/ checkout -B test",
-      "git --git-dir=" . __DIR__ . "/.git/ pull github test",
-    );
-
-    $success = true;
-    $buffer = "";
-
-    foreach ($commands as $cmd) {
-      exec($cmd, $output, $return_value);
-
-      $buffer  .= implode($output) . "\n";
-      $success &= ($return_value == 0);
-
-      if (!$success) {
-        break;
-      }
-    }
-
-    return new Response($buffer, $success ? 200 : 500);
+  return new Response('', $code);
 });
 
-$app->error(function (\Exception $e, $code) use ($app) {
-    if ($app['debug']) {
-        return;
-    }
+$app->post('/', function (Request $request) use ($app) {
+  $request->setTrustedProxies(array('192.168.2.217'));
 
-    return new Response($code);
+  $remoteip    = ip2long($request->getClientIp());
+  $allowednet  = ip2long("192.30.252.0");
+  $allowedmask = ip2long("255.255.252.0");
+
+  if ($remoteip === false || ($remoteip & $allowedmask) != $allowednet) {
+    return new Response("Vous n'êtes pas les serveurs GitHub", 403);
+  }
+
+  $commands = array(
+    "git --git-dir=" . __DIR__ . "/.git/ checkout -B test",
+    "git --git-dir=" . __DIR__ . "/.git/ pull github test",
+  );
+
+  $success = true;
+  $buffer = "";
+
+  foreach ($commands as $cmd) {
+    exec($cmd, $output, $return_value);
+
+    $buffer .= implode("\n", $output) . "\n";
+    $success = $success && ($return_value == 0);
+
+    if (!$success) {
+      break;
+    }
+  }
+
+  return new Response($buffer, $success ? 200 : 500);
 });
 
 $app->run();
